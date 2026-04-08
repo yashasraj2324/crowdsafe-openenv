1cb8de91499081f06145e2527e4dd0f3eb9a9dba
yashasraj2324 <yashasraj245@gmail.com>
2026-04-08 23:37:35 +0530

    fixed

---
+++ server/app.py
@@ -0,0 +1,14 @@
+import uvicorn
+
+
+def run():
+    uvicorn.run(
+        "app.server:app",
+        host="0.0.0.0",
+        port=7860,
+        reload=False,
+    )
+
+
+if __name__ == "__main__":
+    run()
